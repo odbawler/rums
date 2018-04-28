318be714286d4391203e9d15ad62177e33b1b158
Oliver Bawler <odbawler@gmail.com>
2018-04-28 17:31:59 +0100

    debugging for heroku

--- app/main/functions.py
+++ app/main/functions.py
@@ -3,11 +3,21 @@ import math
 
 def calculate_time_worked(end, break_total, start, time_fmt):
     # Subtract break time from finish time
+    print("end")
+    print(end)
+    print("break_total")
+    print(break_total)
+    print("start")
+    print(start)
     end_less_break = datetime.strptime(str(end), time_fmt) - break_total
+    print('end less break')
+    print(end_less_break)
     # Convert time to timedelta type so we can subtract from finish time
     shiftstart = datetime.combine(date.min, start) - datetime.min
     # Subtract the start time from the recalculated finish time to get the overall time worked
     worked = end_less_break - shiftstart
+    print('end - start')
+    print(worked)
     return worked
 
 def calculate_break_time(start_break, end_break, time_fmt):
