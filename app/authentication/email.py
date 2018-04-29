4adc35db5bd04c4c553f2402be94a9b48a8dbf2e
Oliver Bawler <odbawler@gmail.com>
2018-04-29 21:00:25 +0100

    debugging emails

--- app/authentication/email.py
+++ app/authentication/email.py
@@ -3,6 +3,7 @@ from app.email import send_email
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
+    print(current_app.config['ADMINS'][0])
     send_email('[RUMS] Reset Your Password',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
