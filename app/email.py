f92ea449d1118828dcc6181053601764a78819cf
Oliver Bawler <odbawler@gmail.com>
2018-04-29 20:48:48 +0100

    fix password resets

--- app/email.py
+++ app/email.py
@@ -1,5 +1,5 @@
 from flask_mail import Message
-from app import mail
+from app import mail, current_app
 from flask import render_template
 from threading import Thread
 
@@ -12,4 +12,4 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(app._get_current_object(), msg)).start()
+    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
