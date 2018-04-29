35b20b1ce8a20edd01f578877ccac1b7b9680d69
Oliver Bawler <odbawler@gmail.com>
2018-04-29 21:25:26 +0100

    fix password resets

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://localhost/rums'
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = smtp.googlemail.com
+    MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = 1
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
