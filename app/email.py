d5728cc65ea47db73dc417a2aa49f447c5713526
Oliver Bawler <odbawler@gmail.com>
2018-04-29 20:35:57 +0100

    fix password resets

--- app/email.py
+++ app/email.py
@@ -12,4 +12,4 @@ def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    Thread(target=send_async_email, args=(current_app._get_current_object(), msg)).start()
+    Thread(target=send_async_email, args=(app._get_current_object(), msg)).start()
--- app/models.py
+++ app/models.py
@@ -36,12 +36,12 @@ class Employee(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.employee_id, 'exp': time() + expires_in},
-            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+            id = jwt.decode(token, app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
