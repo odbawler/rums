122d585af2fe00064069a414182bcf41f851be9c
Oliver Bawler <odbawler@gmail.com>
2018-04-29 21:23:42 +0100

    fix password resets

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import sendgrid
 import os
+from sendgrid.helpers.mail import *
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -7,9 +9,9 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgres://localhost/rums'
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_SERVER = smtp.googlemail.com
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['odbawler@gmail.com']
