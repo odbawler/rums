52fa6275851be07dc2d6a798bfd73257367f9bc8
Oliver Bawler <odbawler@gmail.com>
2018-04-29 20:43:55 +0100

    fix password resets

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from time import time
 from sqlalchemy import DateTime
+from flask import current_app
 import jwt
 
 class Employee(UserMixin, db.Model):
@@ -36,12 +37,12 @@ class Employee(UserMixin, db.Model):
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
             {'reset_password': self.employee_id, 'exp': time() + expires_in},
-            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+            current_app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
 
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'],
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except:
             return
