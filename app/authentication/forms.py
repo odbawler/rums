a6b32ad3ddcb647765794580b5d8f39c5376c59b
odbawler <Oliver.Bawler@landregistry.gov.uk>
2018-05-15 23:26:57 +0100

    remove commented code

--- app/authentication/forms.py
+++ app/authentication/forms.py
@@ -1,6 +1,5 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-#from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
 from app.models import Employee
 
@@ -48,12 +47,6 @@ class RegisterEmployeeForm(FlaskForm):
         # Check for number
         if not any(char.isdigit() for char in password.data):
             raise ValidationError('Password must contain atleast 1 number.')
-
-#class UsersForm(FlaskForm):
-#    user_list = QuerySelectField(
-#        'Choose User',
-#        query_factory=lambda: Employee.query.all())
-#    submit = SubmitField('Delete')
 
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
